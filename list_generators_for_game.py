b9824e11cc2ae29e6a48dd3a29d305da25116310
dorina <56642774+monberin@users.noreply.github.com>
2019-10-25 15:47:56 +0000

    Update list_generators_for_game.py

--- list_generators_for_game.py
+++ list_generators_for_game.py
@@ -1,3 +1,4 @@
+
 def even_numbers(amount):
     even_list=[]
     for i in range(amount):
@@ -5,42 +6,43 @@ def even_numbers(amount):
             even_list.append(i)
     return even_list
 
-def is_happy(a):
-    result = 0 
-    while(a > 0):
-        x = a%10
-        a //= 10
-        result += x**2
-    return result
-
-def happy_numbers(amount):
-    happy_list = [1]
-    for result in range(2,amount):
-        result1 = result
-        while result1 != 1 and result1 != 4:
-            result1 = is_happy(result1)
-            if result1 == 1:
-                happy_list.append(result)
-                break
-            if result1 == 4:
-                break
-    return happy_list
-
 def lucky_numbers(amount):
     lucky_list = list(range(1, amount + 1, 2)); j = 1
     while lucky_list[j] <= len(lucky_list)-1:
-        lucky_list = [lucky_list[i] for i in range(len(lucky_list)) if (i + 1) % lucky_list[j] != 0]
+        lucky_list = [lucky_list[i] for i in range(len(lucky_list))\
+            if (i + 1) % lucky_list[j] != 0]
         j+=1
     return(lucky_list)
-print(lucky_numbers(100))       
-         
 
-def lucky(number):
-    l = range(1, number + 1, 2)
-    i = 1
-    while i < len(l):
-        del l[l[i] - 1::l[i]]
-        i += 1
-    return l
+import random
+max = 100
+arr = []
+ul_arr = []
+def ulam(amount):
+    s = set()
+    arr.append(1)
+    s.add(1)
 
-print(lucky(100))
+    arr.append(2)
+    s.add(2)
+    for i in range(3, max):
+        count = 0
+        for j in range(0, len(arr)):
+            # Check if i-arr[j] exist in the array or not using set. If yes, Then i can be represented as sum of arr[j] + (i- arr[j])
+            if (i - arr[j]) in s and arr[j] != (i - arr[j]):
+                count += 1
+
+            if count > 2:
+                break
+        if count == 2:
+            arr.append(i)
+            s.add(i)
+    for i in range(amount):
+        n = int(random.uniform(1, 15))
+        x = arr[n - 1]
+        ul_arr.append(x)
+    return ul_arr
+
+if __name__ == "__main__":
+    amount = int(input())
+    print(ulam(amount))
